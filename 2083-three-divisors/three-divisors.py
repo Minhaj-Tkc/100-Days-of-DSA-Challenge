fc65b30caf550ece1dc7a58eaa975c8c0e23ad06
Minhaj Tkc <150417854+Minhaj-Tkc@users.noreply.github.com>
2025-11-03 00:16:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (66.27%) - LeetSync

---
+++ 2083-three-divisors/three-divisors.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+
+        root = int(math.sqrt(n))
+        # n must be a perfect square AND its square root must be a prime number
+        if root * root != n:
+            return False
+        
+        # check if root is prime
+        for i in range(2, int(math.sqrt(root)) + 1):
+            if root % i == 0:
+                return False
+        return root > 1
+
