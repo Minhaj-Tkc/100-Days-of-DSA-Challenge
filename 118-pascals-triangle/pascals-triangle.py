371a325c924ac27ad3b74c995f59f2ddb3badd10
Minhaj Tkc <150417854+Minhaj-Tkc@users.noreply.github.com>
2025-11-27 15:36:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.41%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,15 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res = []
+        finalNums = []
+        finalNums.append([1])  # first row
 
-        for r in range(numRows):
-            # start each row with 1s
-            row = [1] * (r + 1)
+        for i in range(numRows - 1):  # we already have row 1
+            newRow = [1]              # every row starts with 1
 
-            # fill inner values (if any)
-            for c in range(1, r):
-                row[c] = res[r-1][c-1] + res[r-1][c]
+            # fill middle values
+            for j in range(i):
+                newRow.append(finalNums[i][j] + finalNums[i][j+1])
 
-            res.append(row)
+            newRow.append(1)          # every row ends with 1
+            finalNums.append(newRow)
 
-        return res
+        return finalNums
