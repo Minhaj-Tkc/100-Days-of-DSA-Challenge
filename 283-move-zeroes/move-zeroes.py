e1a9fdeb66592ba6fb310e7d865a735666a11352
Minhaj Tkc <150417854+Minhaj-Tkc@users.noreply.github.com>
2025-12-01 23:33:55 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (9.47%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -6,8 +6,12 @@ class Solution:
         k = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[k], nums[i] = nums[i], nums[k]  # swap
+                nums[k] = nums[i]
                 k += 1
+        
+        while k < len(nums):
+            nums[k] = 0
+            k += 1
 
         
 
