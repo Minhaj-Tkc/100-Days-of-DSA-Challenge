2d1897e61e5b01fca2de6fd21d264bd452792b87
Minhaj Tkc <150417854+Minhaj-Tkc@users.noreply.github.com>
2025-12-02 17:08:36 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (57.05%) - LeetSync

--- 2630-alternating-digit-sum/alternating-digit-sum.py
+++ 2630-alternating-digit-sum/alternating-digit-sum.py
@@ -2,14 +2,14 @@ class Solution:
     def alternateDigitSum(self, n: int) -> int:
         sumdigit = 0
         for i, d in enumerate(str(n)):
-            if i % 2 == 0:
+            if i % 2 == 0:      #( "%" ~20–40 CPU Cycles - involves a division inside )
                 sumdigit += int(d)
             else:
                 sumdigit -= int(d)
         
         return sumdigit
 
-# ⭐ Fastest and cleanest
+# ⭐ Fastest and cleanest 
 # class Solution:
 #     def alternateDigitSum(self, n: int) -> int:
 #         s = str(n)
@@ -17,7 +17,7 @@ class Solution:
 #         sign = 1
 #         for ch in s:
 #             total += sign * int(ch)
-#             sign *= -1
+#             sign *= -1   # ( "*" ~3 CPU Cycles )
 #         return total
 
             
