dc8e336d5434bdda8a8b7011c42de759fb030ae8
Minhaj Tkc <150417854+Minhaj-Tkc@users.noreply.github.com>
2025-12-03 13:25:33 +0530

    Time: 6 ms (57.59%) | Memory: 17.7 MB (76.28%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -1,9 +1,11 @@
 class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
-        i = 1  
-        while i < len(words):
-            if sorted(words[i - 1]) == sorted(words[i]):  
-                words.pop(i)  
-            else:
-                i += 1  
-        return words
+        stack = []
+        
+        for w in words:
+            if stack and sorted(stack[-1]) == sorted(w):
+                # They are anagrams → delete current word
+                continue
+            stack.append(w)
+        
+        return stack
