fae2346157d3f3b78b4205b8a1c0ea60befbd58a
Minhaj Tkc <150417854+Minhaj-Tkc@users.noreply.github.com>
2025-12-03 13:34:15 +0530

    Time: 7 ms (54.34%) | Memory: 17.7 MB (95.03%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.py
@@ -2,8 +2,12 @@ class Solution:
     def removeAnagrams(self, words: List[str]) -> List[str]:
         stack = []
         
+        def sig(w):
+            # Signature of word = sorted characters
+            return "".join(sorted(w))
+        
         for w in words:
-            if stack and sorted(stack[-1]) == sorted(w):
+            if stack and sig(stack[-1]) == sig(w):
                 # They are anagrams → delete current word
                 continue
             stack.append(w)
