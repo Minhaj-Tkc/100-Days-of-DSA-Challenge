db1fc7bc07a2b2c25f429b851714f0c2d12a5f87
Minhaj Tkc <150417854+Minhaj-Tkc@users.noreply.github.com>
2025-12-05 14:28:51 +0530

    Time: 31 ms (91.07%) | Memory: 17.9 MB (32.89%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+
+        res = ""
+        m = min(len(word1), len(word2))
+
+        for i in range(m):
+            res += word1[i] + word2[i]
+
+        return res + word1[m:] + word2[m:]
