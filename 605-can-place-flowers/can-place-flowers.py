132eeb046cf7f6eeadc50b9c3c35df6ecff37f4c
Minhaj Tkc <150417854+Minhaj-Tkc@users.noreply.github.com>
2025-12-18 20:55:50 +0530

    Time: 8 ms (51.69%) | Memory: 17.8 MB (99.94%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -3,18 +3,22 @@ class Solution:
         if n == 0:
             return True
 
-        length = len(flowerbed)
-
-        for i in range(length):
+        for i in range(len(flowerbed)):
+            # check current plot
             if flowerbed[i] == 0:
-                left_empty = (i == 0) or (flowerbed[i - 1] == 0)
-                right_empty = (i == length - 1) or (flowerbed[i + 1] == 0)
+                # check left
+                if i > 0 and flowerbed[i - 1] == 1:
+                    continue
 
-                if left_empty and right_empty:
-                    flowerbed[i] = 1
-                    n -= 1
+                # check right
+                if i < len(flowerbed) - 1 and flowerbed[i + 1] == 1:
+                    continue
 
-                    if n == 0:
-                        return True
+                # plant flower
+                flowerbed[i] = 1
+                n -= 1
+
+                if n == 0:
+                    return True
 
         return False
