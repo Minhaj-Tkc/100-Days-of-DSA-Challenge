e235596fc492d5f7ca11a7beca1a5ae215dfec6e
Minhaj Tkc <150417854+Minhaj-Tkc@users.noreply.github.com>
2026-01-09 23:56:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (5.31%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -3,16 +3,16 @@ class Solution:
         stack = []
         next_greater = {}
 
-        # Traverse nums2 from left to right
+        # Step 1: Find next greater for each element in nums2
         for num in nums2:
-            # If current num > top of stack → it is the next greater element
             while stack and num > stack[-1]:
-                next_greater[stack.pop()] = num
+                prev = stack.pop()
+                next_greater[prev] = num
             stack.append(num)
 
-        # Elements left in stack have no next greater element
+        # Step 2: Elements left in stack have no next greater
         while stack:
             next_greater[stack.pop()] = -1
 
-        # Prepare answer for nums1
+        # Step 3: Build answer for nums1
         return [next_greater[num] for num in nums1]
